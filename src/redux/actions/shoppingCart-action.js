8aa9cb4644096410665496746dbcd8f60a142727
ronald davenport <ronalddavenport08@gmail.com>
2021-11-22 12:59:39 -0600

    ron

--- src/redux/actions/shoppingCart-action.js
+++ src/redux/actions/shoppingCart-action.js
@@ -1,58 +1,53 @@
-import { ADD_SHOE_DATA, ADD_TO_CART, GRAB_USERNAME } from "../action-types/getShoeData";
-import { DELETE_FROM_CART } from "../action-types/getShoeData";
+import {
+  ADD_SHOE_DATA,
+  ADD_TO_CART,
+  GRAB_USERNAME,
+} from "../action-types/getShoeData";
 import { FILTER_PROUCTS_BY_BRAND } from "../action-types/getShoeData";
-import ShoeData from "../reducers/ShoeData";
-const { createClient } = require('@supabase/supabase-js')
-const SUPABASE_URL="https://ezwmibduswttopxcmutr.supabase.co"
-const SUPBASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjY1NjIzNSwiZXhwIjoxOTUyMjMyMjM1fQ.T6IwQKPdRMJhBxkq2VOYUxdeOOG4F40rySE0Y0e1Prs"
-const supabase = createClient
- (SUPABASE_URL, 
- SUPBASE_KEY);
 
+export const addToCart = (id, image, brand, name, retailPrice) => {
+  return {
+    type: ADD_TO_CART,
+    payload: id,
+    image,
+    brand,
+    name,
+    retailPrice,
+  };
+};
 
-export const addToCart=(id,image,brand,name,retailPrice)=>{
-    return{
-        type:ADD_TO_CART,
-        payload: id,image,brand,name,retailPrice
-    }
-}
+export const removeFromCart = (id) => {
+  return {
+    type: FILTER_PROUCTS_BY_BRAND,
 
-export const removeFromCart = (id)=>{
-    return{
-        type: FILTER_PROUCTS_BY_BRAND, 
-        
-        payload: id
-        
-        
-    }
-}
+    payload: id,
+  };
+};
 
-export const filterProducts = (products,brand,dispatch) =>{
-    dispatch({
-        type: FILTER_PROUCTS_BY_BRAND,
-        payload: {
-            brand: brand,
-            shoeData : brand == "" ? products: 
-            Object.values(products).filter(x=> x.brand.indexOf(brand)>= 0)
-        }
-    })
+export const filterProducts = (products, brand, dispatch) => {
+  dispatch({
+    type: FILTER_PROUCTS_BY_BRAND,
+    payload: {
+      brand: brand,
+      shoeData:
+        brand == ""
+          ? products
+          : Object.values(products).filter((x) => x.brand.indexOf(brand) >= 0),
+    },
+  });
+};
 
-}
+export const addShoeData = () => async (data) => {
+  return {
+    type: ADD_SHOE_DATA,
 
-export const addShoeData = ()=> async(data)=>{
-    return{
-        type: ADD_SHOE_DATA, 
-        
-        payload: data
-        
-        
-    }
-    
-}
+    payload: data,
+  };
+};
 
-export const grabUsername = (username) =>{
-return({
-    type:GRAB_USERNAME,
-    payload: username
-})
-}
+export const grabUsername = (username) => {
+  return {
+    type: GRAB_USERNAME,
+    payload: username,
+  };
+};
